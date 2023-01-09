5bd6eb4e95ecb40624573b69e51815317098311b
PuraPreguica <erickcelestimo@gmail.com>
2023-01-08 23:53:02 -0300

    Arrumado o problema no docker-compose

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt" //Pacote que mostra as coisas na tela
 
+	"github.com/ErickCelestino/Api_Rest_Em_GO/database"
 	"github.com/ErickCelestino/Api_Rest_Em_GO/models"
 	"github.com/ErickCelestino/Api_Rest_Em_GO/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{Id: 1, Nome: "nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleResquest()
 }
